d2a754f996380b26d394b1b87a1ea618516bd411
kctang <kctang@big2.net>
2020-02-06 22:44:09 +0800

    Fix upgrade issue

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,3 +1,4 @@
+/// <reference path="../global.d.ts" />
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { AppComponent } from './app.component'
